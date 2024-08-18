c352d3333f4851333d528ec9c1922c9330ce685b
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-18 21:23:31 +0300

    Export component

---
+++ my-portfolio/src/components/Team.js
@@ -0,0 +1,192 @@
+export default function Team() {
+    return (
+        <section className="team">
+            <div className="container">
+                <div className="page-section">
+                    <h2 className="page-section__title">MY TEAM</h2>
+                    <img className="page-section__title-style" src="assets/images/title-style.png" alt="" />
+                    <p className="page-section__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <div className="row gutters-40">
+                        <div className="col-md-6 col-lg-3">
+                            <div className="thumbnail">
+                                <img className="img-responsive" src="assets/images/team-1.png" alt="" />
+                                <h4 className="team__single-section__name">VIOLET R. PETERS</h4>
+                                <p className="team__single-section__title">FOUNDER</p>
+
+                                <div className="team__overlay">
+                                    <div className="team__overlay__content">
+                                        <div className="team__single-section--backPage">
+                                            <h4 style={{ paddingLeft: '0;' }} className="team__single-section__name">VIOLET R. PETERS</h4>
+                                            <p style={{ paddingLeft: '0;' }} className="team__single-section__title">FOUNDER</p>
+                                            <p className="team__single-section--backPage__title">Adobe Photoshop <span className="team__single-section--backPage__percent90">90&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '90%', backgroundColor: '#7F56FD' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">Adobe Illustrator <span className="team__single-section--backPage__percent80">80&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '80%', backgroundColor: '#7F56FD' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">After Effect <span className="team__single-section--backPage__percent70">70&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '70%', backgroundColor: '#7F56FD' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <hr style={{ border: '0', borderBottom: '1px solid #ECEFF1', display: 'block', paddingTop: '45px' }} />
+                                            <div className="social-icons">
+                                                <ul>
+                                                    <li><a href="#"><i className="flaticon-facebook-letter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-twitter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-dribbble-logo"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-lg-3">
+                            <div className="thumbnail">
+                                <img src="assets/images/team-3.png" alt="" />
+                                <h4 className="team__single-section__name">PAULA R. MULLINS</h4>
+                                <p className="team__single-section__title">FOUNDER</p>
+                                <div className="team__overlay">
+                                    <div className="team__overlay__content">
+                                        <div className="team__single-section--backPage">
+                                            <h4 style={{ paddingLeft: '0' }} className="team__single-section__name">PAULA R. MULLINS</h4>
+                                            <p style={{ paddingLeft: '0' }} className="team__single-section__title">FOUNDER</p>
+                                            <p className="team__single-section--backPage__title">Adobe Photoshop <span className="team__single-section--backPage__percent90">90&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '90%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">Adobe Illustrator <span className="team__single-section--backPage__percent80">80&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '80%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">After Effect <span className="team__single-section--backPage__percent70">70&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '70%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <hr style={{ border: '0', borderBottom: '1px solid #ECEFF1', display: 'block', paddingTop: '45px;' }} />
+                                            <div className="social-icons">
+                                                <ul>
+                                                    <li><a href="#"><i className="flaticon-facebook-letter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-twitter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-dribbble-logo"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-lg-3">
+                            <div className="thumbnail">
+                                <img src="assets/images/team-2.png" alt="" />
+                                <h4 className="team__single-section__name">LEONARD D. DOWNING</h4>
+                                <p className="team__single-section__title">FOUNDER</p>
+                                <div className="team__overlay">
+                                    <div className="team__overlay__content">
+                                        <div className="team__single-section--backPage">
+                                            <h4 style={{ paddingLeft: '0;' }} className="team__single-section__name">LEONARD D. DOWNING</h4>
+                                            <p style={{ paddingLeft: '0;' }} className="team__single-section__title">FOUNDER</p>
+                                            <p className="team__single-section--backPage__title">Adobe Photoshop <span className="team__single-section--backPage__percent90">90&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '90%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">Adobe Illustrator <span className="team__single-section--backPage__percent80">80&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '80%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">After Effect <span className="team__single-section--backPage__percent70">70&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '70%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <hr style={{ border: '0', borderBottom: '1px solid #ECEFF1', display: 'block', paddingTop: '45px' }} />
+                                            <div className="social-icons">
+                                                <ul>
+                                                    <li><a href="#"><i className="flaticon-facebook-letter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-twitter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-dribbble-logo"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-lg-3">
+                            <div className="thumbnail">
+                                <img src="assets/images/team-3.png" alt="" />
+                                <h4 className="team__single-section__name">PAULA R. MULLINS</h4>
+                                <p className="team__single-section__title">FOUNDER</p>
+                                <div className="team__overlay">
+                                    <div className="team__overlay__content">
+                                        <div className="team__single-section--backPage">
+                                            <h4 style={{ paddingLeft: '0' }} className="team__single-section__name">PAULA R. MULLINS</h4>
+                                            <p style={{ paddingLeft: '0' }} className="team__single-section__title">FOUNDER</p>
+                                            <p className="team__single-section--backPage__title">Adobe Photoshop <span className="team__single-section--backPage__percent90">90&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '90%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">Adobe Illustrator <span className="team__single-section--backPage__percent80">80&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '80%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <p className="team__single-section--backPage__title">After Effect <span className="team__single-section--backPage__percent70">70&#37;</span></p>
+                                            <div className="progress">
+                                                <div className="progress-bar" role="progressbar" style={{ width: '70%', backgroundColor: '#7F56FD;' }}>
+                                                    <span className="progress-bar--dot-main"></span>
+                                                    <span className="progress-bar--dot"></span>
+                                                </div>
+                                            </div>
+                                            <hr style={{ border: '0', borderBottom: '1px solid #ECEFF1', display: 'block', paddingTop: '45px;' }} />
+                                            <div className="social-icons">
+                                                <ul>
+                                                    <li><a href="#"><i className="flaticon-facebook-letter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-twitter-logo"></i></a></li>
+                                                    <li><a href="#"><i className="flaticon-dribbble-logo"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
