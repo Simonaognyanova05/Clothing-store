8c26b17fcdd732880b56daace71579bdf9aaac75
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-20 21:31:50 +0300

    Add edit request to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { loginAdmin } = require('./services/loginAdmin');
+const { welcomeUser } = require('./services/welcomeUser');
+
 
 const app = express();
 
@@ -9,5 +11,9 @@ app.use(express.json());
 
 app.post('/login', async (req, res) => {
     await loginAdmin(req, res);
-})
+});
+
+app.put('/admin/welcome', async (req, res) => {
+    await welcomeUser(req, res);
+});
 app.listen(2005);
