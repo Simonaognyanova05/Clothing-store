91c46dd33925724182018eb2207940f5b9288a65
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-21 13:13:42 +0300

    Add get messages to server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const { getWelcomeData } = require('./services/getWelcomeData');
 const { updateAboutMe } = require('./services/updateAboutMe');
 const { getAboutData } = require('./services/getAboutData');
 const { updateProjects } = require('./services/updateProjects');
+const { addContact } = require('./services/addContact');
+const { getContacts } = require('./services/getContacts');
 
 const app = express();
 
@@ -21,7 +23,7 @@ app.put('/admin/about', async (req, res) => {
     await updateAboutMe(req, res);
 });
 
-app.get('/admin/about', async (req, res) => {
+app.get('/about', async (req, res) => {
     let result = await getAboutData(req, res);
     res.json(result);
 });
@@ -33,8 +35,18 @@ app.put('/admin/projects', async (req, res) => {
 app.put('/admin/welcome', async (req, res) => {
     await welcomeUser(req, res);
 });
-app.get('/admin/welcome', async (req, res) => {
+app.get('/', async (req, res) => {
     let data = await getWelcomeData(req, res);
     res.json(data);
 });
+
+app.post('/contact', async (req, res) => {
+    await addContact(req, res);
+});
+
+app.get('/contact', async (req, res) => {
+    let result = await getContacts(req, res);
+    res.json(result);
+});
+
 app.listen(2005);
