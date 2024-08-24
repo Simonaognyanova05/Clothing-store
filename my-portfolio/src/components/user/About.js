f071daa4bb050cf14ddf5529b21c19ae2b65af15
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-24 14:10:25 +0300

    Change design to about page

--- my-portfolio/src/components/user/About.js
+++ my-portfolio/src/components/user/About.js
@@ -16,14 +16,15 @@ export default function About() {
     return (
         <section id="about">
             <div class="content">
-                <h2>About Me</h2>
-                <img src={data.profileImage} alt="Your Photo" class="profile-photo" />
+                <h1>About Me</h1>
+                <img src={data.profileImage} alt="Your Photo" class="profile-photo"/>
                 <p><strong>Name:</strong> {data.name}</p>
                 <p><strong>Specialty:</strong> {data.specialty}</p>
                 <p><strong>Location:</strong> {data.location}</p>
                 <p><strong>Education:</strong> {data.education}</p>
                 <p>{data.description}</p>
                 <a href="assets/CV.pdf" download class="btn">Download CV</a>
+                <a href="https://github.com/Simonaognyanova05" download class="btn">View My GitHub</a>
             </div>
         </section>
     );
